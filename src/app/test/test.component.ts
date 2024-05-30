66ea7053b18df9c5494aee468c4164a592e7cbfb
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-05-30 11:15:24 +0200

    🔨 Update "test" .ts

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-test',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './test.component.html',
   styleUrl: './test.component.css'
 })
 export class TestComponent {
-
+  faBars = faBars;
 }
