ba380f404e9499aef7047d4e141c8a355c56ad2e
ElKariboosama <tsunaris123456789@gmail.com>
2024-05-30 08:51:53 +0200

    add a test route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"test",component:TestComponent
+    }
+];
