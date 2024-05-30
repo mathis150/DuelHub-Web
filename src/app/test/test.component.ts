a85d1d840b7da48041f367c22321ba77ac6fb132
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-05-30 15:33:57 +0200

    💄Navbar responsive finished

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-test',
@@ -11,4 +11,10 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 export class TestComponent {
   faBars = faBars;
+  faX = faXmark;
+  show_navbar = false;
+
+  mobileNavBar() {
+    this.show_navbar = !this.show_navbar;
+  }
 }
