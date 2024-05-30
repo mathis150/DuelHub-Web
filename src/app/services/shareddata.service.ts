6eed38eb92a372e7ffc1f86d391c8f78638e386e
ElKariboosama <tsunaris123456789@gmail.com>
2024-05-30 09:23:50 +0200

    add documentation to imported services

--- src/app/services/shareddata.service.ts
+++ src/app/services/shareddata.service.ts
@@ -24,6 +24,9 @@ class KSubscription {
 @Injectable({
   providedIn: 'root',
 })
+/**
+ * Handle the gestion of global variables throughout your application
+ */
 export class DataService {
     public _subscriptions : Map<string,KSubscription> = new Map<string,KSubscription>()
 
@@ -40,13 +43,45 @@ export class DataService {
       return this._subscriptions.get(name)!
     }
   
-    get(name: string) : KSubscription{
+    private get(name: string) : KSubscription{
       if (this.duplicateCheck(name)) {
         return this._subscriptions.get(name)!
       }
       return this.addSubscription(name)
     }
 
+    /**
+     * Set the global variable defined by the given name to a value
+     * @param name name of the global variable
+     * @param value the new value to set the global variable to
+     */
+    next(name: string, value: any) {
+      if (this.duplicateCheck(name)) {
+        this._subscriptions.get(name)!.next(value)
+      } else {
+        this.addSubscription(name).next(value)
+      }
+    }
+
+    /**
+     * Get the value of the global variable defined by the given name
+     * @param name name of the global variable
+     * @returns the value if the variable exist or null if it dosnt
+     */
+    value(name: string) {
+      if (this.duplicateCheck(name)) {
+        return this._subscriptions.get(name)!
+      }
+      return null
+    }
+
+    /**
+     * Subscribe to a global variable given its name, the given function is run as a callback
+     * when the variable is changed using the next() function
+     * @param name name of the global variable
+     * @param func the callback function to call when the variable change
+     * @returns the Subscription (keep it locally to unsubscribe when needed)
+     */
     subscribe(name: string, func : (data : any) => void) : Subscription {
       return this.get(name).subscribe(func)
     }
--- src/app/services/updater.service.ts
+++ src/app/services/updater.service.ts
@@ -20,6 +20,10 @@ class Updater {
 @Injectable({
   providedIn: 'root',
 })
+/**
+ * Handle the synchronisation of different part of your application, 
+ * allows the user to run different function directly from one call
+ */
 export class UpdateService {
     public _updaters : Map<string,Updater> = new Map<string,Updater>()
 
@@ -27,7 +31,7 @@ export class UpdateService {
       return this._updaters.has(name)
     }
   
-    get(name: string) : Updater{
+    private get(name: string) : Updater{
       if (this.duplicateCheck(name)) {
         return this._updaters.get(name)!
       } else {
@@ -36,10 +40,21 @@ export class UpdateService {
       }
     }
 
+    /**
+     * Subscribe to the updater with the given name, the given function will be run as a callback
+     * when the updater is updated
+     * @param name name of the updater
+     * @param func the callback function to call when updated
+     * @returns the Subscription (keep it locally to unsubscribe when needed)
+     */
     subscribe(name: string, func : () => void) {
       return this.get(name).subscribe(func)
     }
 
+    /**
+     * Run all function subscribed to the updater defined by the given name
+     * @param name name of the global variable
+     */
     update(name : string) {
       if (this.duplicateCheck(name)) {
         this._updaters.get(name)!.update()
