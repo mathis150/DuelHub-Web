5737cb9ae71308267b9e70e46f0ae7e458ff18df
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-07-10 11:50:19 +0200

    🔧Update of Routes Configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForumComponent } from './forum/forum.component';
+import { WikiComponent } from './wiki/wiki.component';
 
 export const routes: Routes = [
     { path:"", component:HomeComponent },
+    { path:"login", component:LoginComponent },
+    { path:"register", component:RegisterComponent },
+    { path:"forum", component:ForumComponent },
+    { path:"wiki", component:WikiComponent }
 ];
