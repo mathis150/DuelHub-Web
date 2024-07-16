5595349545bd44aba1ee674c32e693188ab1fa4b
ElKariboosama <tsunaris123456789@gmail.com>
2024-07-16 14:42:39 +0200

    🔨Update header to use new database scheme

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { ApiService, FeaturedGame } from '../services/api.service';
 
 @Component({
   selector: 'app-header',
@@ -9,21 +10,31 @@ import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   faBars = faBars;
   faX = faXmark;
-
   is_home = false;
   show_navbar = false;
 
+  featuredGames : FeaturedGame[] = []; 
+  
   page = window.location.pathname;
 
-  constructor() {
+  constructor(
+    protected _api : ApiService
+  ) {
     if(window.location.pathname == '/') {
       this.is_home = true;
     }
   }
-
+  
+  ngOnInit(): void {
+    this._api.GetHomePageFeaturedGameList().then((games) => {
+      console.table(games)
+      this.featuredGames = games
+    })
+  }
+  
   mobileNavBar() {
     this.show_navbar = !this.show_navbar;
   }
