41820571a931a61a17f88d586f35f3bfdfe20e5a
ElKariboosama <tsunaris123456789@gmail.com>
2024-07-16 13:35:09 +0200

    🔨Add homepage api functions

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,48 @@
 import { Injectable } from '@angular/core';
 
+interface FeaturedGame {
+  title: string,
+  routing: string,
+  bannerSRC: string,
+}
+
+interface Game {
+  title: string,
+  routing: string,
+  coverSRC: string,
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class ApiService {
   public apiServer : string = 'http://10.31.33.46:8000'
-  public filePath : string = this.apiServer + '/file'
-  public imagePath : string = this.apiServer + '/file/image/'
-  public pagePath : string = this.apiServer + '/file/pages/'
+
+  constructor() {}
+
+  GetHomePageFeaturedGames(): Promise<FeaturedGame[]> {
+    return fetch(this.apiServer + "/game/featured")
+      .then((fetch) => {
+        return fetch.json()
+      })
+      .then((games) => {
+        if (games.length == 0) {
+          return [];
+        }
+        return games
+      })
+  }
+
+  GetHomePagePopularGameList(): Promise<Game[]> {
+    return fetch(this.apiServer + "/game/popular/players")
+      .then((fetch) => {
+        return fetch.json()
+      })
+      .then((games) => {
+        if (games.length == 0) {
+          return []
+        }
+        return games;
+      })
+  }
 }
