a3a63b73f110ed64c6253a1d783ac4716aab85b5
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-07-17 09:20:33 +0200

    🔧Update routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ForumComponent } from './forum/forum.component';
 import { WikiComponent } from './wiki/wiki.component';
+import { ForumPostsComponent } from './forum-posts/forum-posts.component';
+import { ForumPostComponent } from './forum-post/forum-post.component';
 
 export const routes: Routes = [
     { path:"", component:HomeComponent },
     { path:"login", component:LoginComponent },
     { path:"register", component:RegisterComponent },
     { path:"forum", component:ForumComponent },
+    { path:"forum/all-posts", component:ForumPostsComponent },
+    { path:"forum/post", component:ForumPostComponent },
     { path:"wiki", component:WikiComponent }
 ];
