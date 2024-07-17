a999ea02eb433aa144ac094b21d078f18dbd03ea
ElKariboosama <tsunaris123456789@gmail.com>
2024-07-17 09:54:42 +0200

    Modify header to have featured game banners

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { ApiService, FeaturedGame } from '../services/api.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit,OnDestroy {
   faBars = faBars;
   faX = faXmark;
   is_home = false;
   show_navbar = false;
 
-  featuredGames : FeaturedGame[] = []; 
+  featuredGames : FeaturedGame[] = [];
+  currentPreview : number = 0;
+  featuredRotationTimer! : number; 
+  FEATURED_ROTATION_TIME = 3000;
   
   page = window.location.pathname;
 
@@ -33,6 +37,17 @@ export class HeaderComponent implements OnInit {
       console.table(games)
       this.featuredGames = games
     })
+    
+    clearInterval(this.featuredRotationTimer);
+    // @ts-ignore
+    this.featuredRotationTimer = setInterval(() => {
+      this.currentPreview++;
+      if (this.currentPreview == this.featuredGames.length) this.currentPreview = 0;
+    },this.FEATURED_ROTATION_TIME)
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.featuredRotationTimer)
   }
   
   mobileNavBar() {
