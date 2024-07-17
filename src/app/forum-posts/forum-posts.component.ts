da44c94e6b7e39f60faa83400c2857f2094a3e25
Mathis LENOIR <48561648+mathis150@users.noreply.github.com>
2024-07-17 09:20:11 +0200

    🔨Add Forum Posts components

---
+++ src/app/forum-posts/forum-posts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-posts',
+  standalone: true,
+  imports: [],
+  templateUrl: './forum-posts.component.html',
+  styleUrl: './forum-posts.component.css'
+})
+export class ForumPostsComponent {
+
+}
